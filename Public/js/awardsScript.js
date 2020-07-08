54598b331c5ee2601a531b768c15a3ef3fbbc3f2
Ozukyo <ozukyo@gmail.com>
2020-07-08 18:49:37 +0200

    Add reading award cards from database

---
+++ Public/js/awardsScript.js
@@ -0,0 +1,54 @@
+const container = document.querySelector(".container");
+
+function getAwards() {
+    fetch(`http://localhost:8003/awards`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(awards){
+            innerAwards(awards)
+
+        })
+}
+
+function innerAwards(awards) {
+    let cards = document.createElement("div");
+    cards.className = "cards";
+    
+    console.log(cards);
+
+    awards.forEach(award => {
+        let cardElement = document.createElement("div");
+        cardElement.className = "card";
+
+        let cardTitle = document.createElement("div");
+        cardTitle.className = "card-title";
+        cardTitle.innerText = award.title;
+
+        let cardImage = document.createElement("div");
+        cardImage.className = "card-image";
+        let img = document.createElement("img");
+        img.src = "../../resoruces/awardLogo/" + award.imageSrc;
+        cardImage.appendChild(img);
+
+        let cardDescription = document.createElement("div");
+        cardDescription.className = "card-description";
+        cardDescription.innerText = award.description;
+
+        let cardPills = document.createElement("div");
+        cardPills.className = "card-pillPrice";
+        cardPills.innerText = "cost: " + award.price + " p";
+
+        cardElement.appendChild(cardTitle);
+        cardElement.appendChild(cardImage);
+        cardElement.appendChild(cardDescription);
+        cardElement.appendChild(cardPills);
+        cards.appendChild(cardElement);
+        container.appendChild(cards);
+
+    });
+
+}
+
+
+getAwards();
